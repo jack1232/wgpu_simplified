f166d5154f9e42557bdc09c67235afe6c14e3ff3
jack1232 <jackjack5810@yahoo.com>
2023-10-19 11:30:15 -0400

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,89 @@
 use winit::window::Window;
-use std::path::PathBuf;
+//use std::path::PathBuf;
+use std::f32::consts::PI;
+use cgmath::*;
+
+#[rustfmt::skip]
+#[allow(unused)]
+pub const OPENGL_TO_WGPU_MATRIX: Matrix4<f32> = Matrix4::new(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 0.5, 0.0,
+    0.0, 0.0, 0.5, 1.0,
+);
+
+pub fn create_model_mat(translation:[f32; 3], rotation:[f32; 3], scaling:[f32; 3]) -> Matrix4<f32> {
+    // create transformation matrices
+    let trans_mat = Matrix4::from_translation(Vector3::new(translation[0], translation[1], translation[2]));
+    let rotate_mat_x = Matrix4::from_angle_x(Rad(rotation[0]));
+    let rotate_mat_y = Matrix4::from_angle_y(Rad(rotation[1]));
+    let rotate_mat_z = Matrix4::from_angle_z(Rad(rotation[2]));
+    let scale_mat = Matrix4::from_nonuniform_scale(scaling[0], scaling[1], scaling[2]);
+
+    // combine all transformation matrices together to form a final transform matrix: model matrix
+    let model_mat = trans_mat * rotate_mat_z * rotate_mat_y * rotate_mat_x * scale_mat;
+
+    // return final model matrix
+    model_mat
+}
+
+pub fn create_view_mat(camera_position: Point3<f32>, look_direction: Point3<f32>, up_direction: Vector3<f32>) -> Matrix4<f32> {
+    Matrix4::look_at_rh(camera_position, look_direction, up_direction)
+}
+
+pub fn create_projection_mat(aspect:f32, is_perspective:bool) -> Matrix4<f32> {
+    let project_mat:Matrix4<f32>;
+    if is_perspective {
+        project_mat = OPENGL_TO_WGPU_MATRIX * perspective(Rad(2.0*PI/5.0), aspect, 0.1, 1000.0);
+    } else {
+        project_mat = OPENGL_TO_WGPU_MATRIX * ortho(-4.0, 4.0, -3.0, 3.0, -1.0, 6.0);
+    }
+    project_mat
+}
+
+pub fn create_perspective_mat(fovy:Rad<f32>, aspect:f32, near: f32, far:f32) -> Matrix4<f32> {
+    OPENGL_TO_WGPU_MATRIX * perspective(fovy, aspect, near, far)
+}
+
+pub fn create_ortho_mat(left: f32, right: f32, bottom: f32, top: f32, near: f32, far: f32) -> Matrix4<f32> {
+    OPENGL_TO_WGPU_MATRIX * ortho(left, right, bottom, top, near, far)    
+}
+
+pub fn create_vp_ortho_mat(left: f32, right: f32, bottom: f32, top: f32, near: f32, far: f32, camera_position: Point3<f32>, 
+    look_direction: Point3<f32>, up_direction: Vector3<f32>) -> (Matrix4<f32>, Matrix4<f32>, Matrix4<f32>) {
+    
+    // construct view matrix
+    let view_mat = Matrix4::look_at_rh(camera_position, look_direction, up_direction);     
+
+    // construct projection matrix
+    let project_mat = OPENGL_TO_WGPU_MATRIX * ortho(left, right, bottom, top, near, far);    
+    
+    // contruct view-projection matrix
+    let vp_mat = project_mat * view_mat;
+   
+    // return various matrices
+    (view_mat, project_mat, vp_mat)
+} 
+
+pub fn create_vp_mat(camera_position: Point3<f32>, look_direction: Point3<f32>, up_direction: Vector3<f32>,
+    aspect:f32) -> (Matrix4<f32>, Matrix4<f32>, Matrix4<f32>) {
+    
+    // construct view matrix
+    let view_mat = Matrix4::look_at_rh(camera_position, look_direction, up_direction);     
+
+    // construct projection matrix
+    let project_mat = OPENGL_TO_WGPU_MATRIX * perspective(Rad(2.0*PI/5.0), aspect, 0.1, 1000.0);
+   
+    // contruct view-projection matrix
+    let vp_mat = project_mat * view_mat;
+   
+    // return various matrices
+    (view_mat, project_mat, vp_mat)
+} 
+
+pub fn create_projection_ortho(left: f32, right: f32, bottom: f32, top: f32, near: f32, far: f32) -> Matrix4<f32> {
+    OPENGL_TO_WGPU_MATRIX * ortho(left, right, bottom, top, near, far)    
+}
 
 pub fn create_color_attachment<'a>(texture_view: &'a wgpu::TextureView) -> wgpu::RenderPassColorAttachment<'a> {
     wgpu::RenderPassColorAttachment {
